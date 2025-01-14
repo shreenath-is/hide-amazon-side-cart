b171a8fe872fd71ee582da02e97dc1d00ca5e885
Shreenath Vyas <shreenathvyas05@gmail.com>
2025-01-14 07:33:17 +0000

    added observer for dynamic cart element

--- content.js
+++ content.js
@@ -1,7 +1,42 @@
-// Select the cart sidebar element by its ID
-const cartSidebar = document.querySelector('#nav-flyout-ewc');
+console.log("Content script loaded for Amazon domains.");
 
-// Hide the sidebar if it exists
-if (cartSidebar) {
-  cartSidebar.style.display = 'none';
+let hideCount = 0
+
+function hideCartSidebar() {
+  const cartSidebar = document.querySelector('#nav-flyout-ewc');
+  if (cartSidebar) {
+    cartSidebar.style.display = 'none';
+
+    hideCount++
+
+    const parent = cartSidebar.parentNode;
+    if (parent) {
+      parent.style.margin = '0';
+      parent.style.padding = '0';
+      parent.style.height = 'auto';
+      parent.style.overflow = 'visible';
+    }
+
+    console.log("Cart sidebar hidden.");
+  }
 }
+
+hideCartSidebar();
+
+const observer = new MutationObserver(() => {
+  hideCartSidebar();
+});
+
+const parent = document.querySelector('#navbar')
+
+observer.observe(parent, {
+  childList: true,
+  subtree: true,
+});
+
+
+if (hideCount > 1) {
+  console.log('Removing sidebar observer')
+  observer.disconnect();
+}
+
